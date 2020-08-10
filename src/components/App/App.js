5341cfc76891b325c0bfa9a56c0e21fabacf7a7c
Robert Perillo <perillo.robert@gmail.com>
2020-08-10 12:42:57 -0600

    create navbar using material ui

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import LandingPage from '../LandingPage/LandingPage'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <LandingPage />
       </header>
     </div>
   );
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import Drawer from '@material-ui/core/Drawer';
+import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+
+
+function Navbar() {
+    const [state, setState] = useState({
+        menu: false,
+    });
+
+    const toggleDrawer = (open) => (event) => {
+        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+            return;
+        }
+        setState({ ...state, menu: open });
+    };
+
+    const list = () => (
+        <div
+            style={{width: '300px'}}
+            onClick={toggleDrawer('menu', false)}
+            onKeyDown={toggleDrawer('menu', false)}
+        >
+            <List>
+                {['Projects', 'Resume', 'Profile'].map((text) => (
+                    <ListItem button key={text}>
+                        <ListItemIcon><MailIcon /></ListItemIcon>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                ))}
+            </List>
+        </div>
+    );
+
+    return (
+        <div>
+                <React.Fragment>
+                    <Button onClick={toggleDrawer(true)}>Menu</Button>
+                    <Drawer anchor={'top'} open={state['menu']} onClose={toggleDrawer(false)}>
+                        {list('menu')}
+                    </Drawer>
+                </React.Fragment>
+        </div>
+    );
+}
+
+export default Navbar;
