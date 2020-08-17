91252e9575deca0fdb968e018830097a6d682273
Robert Perillo <perillo.robert@gmail.com>
2020-08-17 17:17:26 -0600

    add carousel to project page

--- src/components/ProjectImages/ProjectImages.jsx
+++ src/components/ProjectImages/ProjectImages.jsx
@@ -1,6 +1,18 @@
 import React from "react";
 import './ProjectImages.css'
 
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import MobileStepper from '@material-ui/core/MobileStepper';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+import SwipeableViews from 'react-swipeable-views';
+import { autoPlay } from 'react-swipeable-views-utils';
+
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+
 const projectImages = [
     {
         laptopImg: 'https://i.imgur.com/aFXN6ZW.png',
@@ -35,17 +47,136 @@ const projectImages = [
 ]
 
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: '80%',
+        flexGrow: 1,
+    },
+    header: {
+        display: 'flex',
+        alignItems: 'center',
+        height: 50,
+        paddingLeft: theme.spacing(4),
+        backgroundColor: theme.palette.background.default,
+    },
+    img: {
+        // height: 500,
+        display: 'block',
+        maxWidth: 700,
+        overflow: 'hidden',
+        width: '100%',
+    },
+}));
+
 function ProjectImages() {
+    const classes = useStyles();
+    const theme = useTheme();
+    const [activeStep, setActiveStep] = React.useState(0);
+    const maxSteps = projectImages.length;
+
+    const handleNext = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    };
+
+    const handleBack = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    };
+
+    const handleStepChange = (step) => {
+        setActiveStep(step);
+    };
     return (
         <>
-            {projectImages.map(project =>
-                <div className='flex-container'>
-                    <img src={project.laptopImg} className='laptop-img'></img>
-                    <img src={project.mobileImg} className='mobile-img'></img>
-                </div>
-            )}
+            <div className={classes.root}>
+                <Paper square elevation={0} className={classes.header}>
+                    <Typography>{projectImages[activeStep].desc}</Typography>
+                </Paper>
+                <AutoPlaySwipeableViews
+                    axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+                    index={activeStep}
+                    onChangeIndex={handleStepChange}
+                    enableMouseEvents
+                >
+                    {projectImages.map((step, index) => (
+                        <div key={index}>
+                            {Math.abs(activeStep - index) <= 2 ? (
+                                <div className="flex-container">
+                                    <img className={classes.img} src={step.laptopImg} />
+                                    <img className='mobile-img' src={step.mobileImg} />
+                                </div>
+                            ) : null}
+                        </div>
+                    ))}
+                </AutoPlaySwipeableViews>
+                <MobileStepper
+                    steps={maxSteps}
+                    position="static"
+                    variant="text"
+                    activeStep={activeStep}
+                    nextButton={
+                        <Button size="small" onClick={handleNext} disabled={activeStep === maxSteps - 1}>
+                            Next
+            {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+                        </Button>
+                    }
+                    backButton={
+                        <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+                            {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+            Back
+          </Button>
+                    }
+                />
+            </div>
         </>
     )
 }
 
 export default ProjectImages;
+
+
+
+
+
+// function SwipeableTextMobileStepper() {
+
+
+//     return (
+//         <div className={classes.root}>
+//             <Paper square elevation={0} className={classes.header}>
+//                 <Typography>{tutorialSteps[activeStep].label}</Typography>
+//             </Paper>
+//             <AutoPlaySwipeableViews
+//                 axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+//                 index={activeStep}
+//                 onChangeIndex={handleStepChange}
+//                 enableMouseEvents
+//             >
+//                 {tutorialSteps.map((step, index) => (
+//                     <div key={step.label}>
+//                         {Math.abs(activeStep - index) <= 2 ? (
+//                             <img className={classes.img} src={step.imgPath} alt={step.label} />
+//                         ) : null}
+//                     </div>
+//                 ))}
+//             </AutoPlaySwipeableViews>
+//             <MobileStepper
+//                 steps={maxSteps}
+//                 position="static"
+//                 variant="text"
+//                 activeStep={activeStep}
+//                 nextButton={
+//                     <Button size="small" onClick={handleNext} disabled={activeStep === maxSteps - 1}>
+//                         Next
+//             {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+//                     </Button>
+//                 }
+//                 backButton={
+//                     <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+//                         {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+//             Back
+//           </Button>
+//                 }
+//             />
+//         </div>
+//     );
+// }
