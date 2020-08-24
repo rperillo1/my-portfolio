80c5d0baf3c0914c4a4e7d6db90a3cd8f1c5d162
Robert Perillo <perillo.robert@gmail.com>
2020-08-24 14:29:12 -0400

    add logos to projects page

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
-// import './ProjectSlides.css'
+import './Projects.css'
 
 
 const projectImages = [
@@ -8,7 +8,7 @@ const projectImages = [
         laptopImg: 'https://i.imgur.com/aFXN6ZW.png',
         mobileImg: 'https://i.imgur.com/AEZyr7b.png',
         desc: 'D&D Character Manager. Track your character stats, spells, abilities, money and more! Technologies: Node, Express, React, MongoDB, Mongoose, HTML, CSS, Bootstrap, D&D 5e Players Handbook API',
-        logos: ['https://i.imgur.com/hWrPRuJ.png', 'https://i.imgur.com/DCsmNit.png', 'https://i.imgur.com/oyV9d35.png', 'https://i.imgur.com/MoLOBzA.png', 'https://i.imgur.com/u0fDtEb.png', 'https://i.imgur.com/p14Spbf.png', 'https://i.imgur.com/P2DsTfc.png']
+        logos: ['https://i.imgur.com/hWrPRuJ.png', 'https://i.imgur.com/DCsmNit.png', 'https://i.imgur.com/oyV9d35.png', 'https://i.imgur.com/MoLOBzA.png', 'https://i.imgur.com/u0fDtEb.png', 'https://i.imgur.com/p14Spbf.png', 'https://i.imgur.com/GMHSzcr.png']
     },
     {
         laptopImg: 'https://i.imgur.com/cSkIKGT.png',
@@ -20,25 +20,25 @@ const projectImages = [
         laptopImg: 'https://i.imgur.com/moxIop7.png',
         mobileImg: 'https://i.imgur.com/aOpRNab.png',
         desc: 'A gaming scheduling app. Sign up via google OAuth. Create friend groups and games within those groups. Then create polls of dates for your group members to vote on! Technologies: Express, Node, Mongoose, MongoDB, EJS, HTML, CSS, Materialize, Google Calendar API',
-        logos: []
+        logos: ['https://i.imgur.com/DCsmNit.png', 'https://i.imgur.com/oyV9d35.png', 'https://i.imgur.com/MoLOBzA.png', 'https://i.imgur.com/u0fDtEb.png', 'https://i.imgur.com/p14Spbf.png', 'https://i.imgur.com/aajFadH.png']
     },
     {
         laptopImg: 'https://i.imgur.com/1Noyanj.png',
         mobileImg: 'https://i.imgur.com/YOTeiCy.png',
         desc: 'Mobile responsive slot machine made with vanilla JavaScript, HTML & CSS',
-        logos: []
+        logos: ['https://i.imgur.com/DCsmNit.png', 'https://i.imgur.com/EnsYkPa.png']
     },
     {
         laptopImg: 'https://i.imgur.com/HrcV3nI.png',
         mobileImg: null,
         desc: 'A virtual dog show app. Allows you to sign in via google OAuth, create a dog profile yout dog to competitions for people to vote. Technologies: Express, Node, EJS templates, Javascript, Mongoose, MongoDB, HTML, CSS',
-        logos: []
+        logos: ['https://i.imgur.com/DCsmNit.png', 'https://i.imgur.com/oyV9d35.png', 'https://i.imgur.com/MoLOBzA.png', 'https://i.imgur.com/u0fDtEb.png', 'https://i.imgur.com/p14Spbf.png', 'https://i.imgur.com/EnsYkPa.png']
     },
     {
         laptopImg: 'https://i.imgur.com/RpK5zxW.png',
         mobileImg: null,
         desc: 'Single Player RPG made with vanilla JavaScript, HTML, CSS (Grid, FLexbox, Bootstrap)',
-        logos: []
+        logos: ['https://i.imgur.com/DCsmNit.png', 'https://i.imgur.com/EnsYkPa.png']
     },
 ]
 
@@ -56,7 +56,7 @@ function Projects() {
     const classes = useStyles();
 
     return (
-        <div>
+        <div className='projects-container'>
             {projectImages.map((step, index) => (
                 <div key={index}>
                     <div className="flex-container">
@@ -65,9 +65,11 @@ function Projects() {
                             : <img className={classes.img} className='mobile-img' src={step.mobileImg} alt={`mobile project ${index}`} />}
                     </div>
                     <p>{step.desc}</p>
-                     { step.logos.map(logo => 
-                        <img src={logo} className='' />
-                    )}
+                    <div className='logos'>
+                        {step.logos.map(logo =>
+                            <img src={logo} />
+                        )}
+                    </div>
                 </div>
             ))}
         </div>
