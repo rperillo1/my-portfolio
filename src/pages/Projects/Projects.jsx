b6da61a360ad35a5bdc56b7eb7cfb74f7bc3efe1
Robert Perillo <perillo.robert@gmail.com>
2020-08-25 17:33:32 -0400

    update color scheme

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -56,7 +56,7 @@ function Projects() {
                 <div className='carousel' >
                     <h1 id='title'>{project.title}</h1>
                     <div>
-                        <Carousel autoPlay={true} infiniteLoop={true} showIndicators={true}>
+                        <Carousel autoPlay={true} infiniteLoop showIndicators emulateTouch>
                             {project.images.map(image =>
                                 <div>
                                     <img src={image} />
@@ -80,45 +80,5 @@ function Projects() {
 }
 
 export default Projects;
-
-
-// const projectImages = [
-//     {
-//         laptopImg: 'https://i.imgur.com/aFXN6ZW.png',
-//         mobileImg: 'https://i.imgur.com/AEZyr7b.png',
-//         desc: 'D&D Character Manager. Track your character stats, spells, abilities, money and more! Pulls spell information from the D&D 5e Players Handbook API',
-//         logos: ['https://i.imgur.com/hWrPRuJ.png', 'https://i.imgur.com/DCsmNit.png', 'https://i.imgur.com/oyV9d35.png', 'https://i.imgur.com/MoLOBzA.png', 'https://i.imgur.com/u0fDtEb.png', 'https://i.imgur.com/p14Spbf.png', 'https://i.imgur.com/GMHSzcr.png']
-//     },
-//     {
-//         laptopImg: 'https://i.imgur.com/cSkIKGT.png',
-//         mobileImg: 'https://i.imgur.com/X4MSt5Y.png',
-//         desc: 'Job Search Tracker! Track job application notes, company information, interest levels. Uses GitHubs jobs API to pull relevant job info.',
-//         logos: ['https://i.imgur.com/a6uI0Vs.png', 'https://i.imgur.com/IGsHeNw.png', 'https://i.imgur.com/znLwmfG.png', 'https://i.imgur.com/6xfuexp.png', 'https://i.imgur.com/DCsmNit.png', 'https://i.imgur.com/UHCbPD3.png']
-//     },
-//     {
-//         laptopImg: 'https://i.imgur.com/moxIop7.png',
-//         mobileImg: 'https://i.imgur.com/aOpRNab.png',
-//         desc: 'A gaming scheduling app. Sign up via google OAuth. Create friend groups and games within those groups. Then create polls of dates for your group members to vote on! Uses the Google Calendar API so that you can post an event date that was chosen by your friends.',
-//         logos: ['https://i.imgur.com/DCsmNit.png', 'https://i.imgur.com/oyV9d35.png', 'https://i.imgur.com/MoLOBzA.png', 'https://i.imgur.com/u0fDtEb.png', 'https://i.imgur.com/p14Spbf.png', 'https://i.imgur.com/aajFadH.png']
-//     },
-//     {
-//         laptopImg: 'https://i.imgur.com/1Noyanj.png',
-//         mobileImg: 'https://i.imgur.com/YOTeiCy.png',
-//         desc: 'Mobile responsive slot machine.',
-//         logos: ['https://i.imgur.com/DCsmNit.png', 'https://i.imgur.com/EnsYkPa.png']
-//     },
-//     {
-//         laptopImg: 'https://i.imgur.com/HrcV3nI.png',
-//         mobileImg: null,
-//         desc: 'A virtual dog show app. Allows you to sign in via google OAuth, create a dog profile to add your dog to competitions for users to vote.',
-//         logos: ['https://i.imgur.com/DCsmNit.png', 'https://i.imgur.com/oyV9d35.png', 'https://i.imgur.com/MoLOBzA.png', 'https://i.imgur.com/u0fDtEb.png', 'https://i.imgur.com/p14Spbf.png', 'https://i.imgur.com/EnsYkPa.png']
-//     },
-//     {
-//         laptopImg: 'https://i.imgur.com/RpK5zxW.png',
-//         mobileImg: null,
-//         desc: 'Single Player RPG. Create your character, choose a set of skills, go adventure and if you get through the forest and mountains and beat all the creatures before you lose all your life, YOU WIN! ',
-//         logos: ['https://i.imgur.com/DCsmNit.png', 'https://i.imgur.com/EnsYkPa.png']
-//     },
-// ]
 
 
--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -28,18 +28,6 @@ function Resume() {
         setChecked((prev) => !prev);
     };
 
-    // useEffect(() => {
-    //     const timer = setTimeout(() => {
-    //       console.log('This will run after 1 second!')
-    //     }, 1000);
-    //     return () => clearTimeout(timer);
-    //   }, []);
-    
-    // fadeInTimout = () => {
-    //     const timer = setTimeout(() => {
-    //         console.log('This will run after 1 second!')
-    //       }, 1000);
-    // }
 
     return (
         <div id='resume-page'>
